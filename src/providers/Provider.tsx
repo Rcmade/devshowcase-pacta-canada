cd9dab038e39b4a16a894f79116d00232233b08f
Rcmade <rahulchourasiya4567@gmail.com>
2025-07-29 21:00:09 +0530

    resolve suspence boudry issue

--- src/providers/Provider.tsx
+++ src/providers/Provider.tsx
@@ -5,6 +5,7 @@ import { SessionProvider } from "next-auth/react";
 import { AlertDialog } from "@/components/alerts/AlertDialog";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Suspense } from "react";
 import { Toaster } from "sonner";
 
 const queryClient = new QueryClient({
@@ -18,14 +19,16 @@ const queryClient = new QueryClient({
 const Provider = ({ children }: Children) => {
   return (
     <>
-      <SessionProvider>
-        <QueryClientProvider client={queryClient}>
-          {children}
-          <ReactQueryDevtools initialIsOpen={false} />
-          <Toaster position="top-center" />
-          <AlertDialog />
-        </QueryClientProvider>
-      </SessionProvider>
+      <Suspense>
+        <SessionProvider>
+          <QueryClientProvider client={queryClient}>
+            {children}
+            <ReactQueryDevtools initialIsOpen={false} />
+            <Toaster position="top-center" />
+            <AlertDialog />
+          </QueryClientProvider>
+        </SessionProvider>
+      </Suspense>
     </>
   );
 };
